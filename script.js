b436b483a9eaea7f0578bb35c1351f5d0f1e00c7
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-06-08 20:55:28 +0330

    some modifications

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 let boxes = document.querySelectorAll(".box");
-boxes = Array.from(boxes);
-
-console.log(boxes[0].classList[1]);
 
 boxes.forEach((box) => {
   box.addEventListener("mouseover", (e) => {
-    console.log(e.target);
     box.classList.add("active");
   });
   box.addEventListener("mouseout", (e) => {
-    console.log(e.target);
     box.classList.remove("active");
   });
 });
